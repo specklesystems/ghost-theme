1ca29605e39ca85f5b463807acc662a299ed763e
Dimitrie Stefanescu <didimitrie@gmail.com>
2021-01-31 23:27:38 +0000

    feat(wow): wow
    wwwwooooow

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,23 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
-      height: theme => ( {
+      height: theme => ({
         'screen/2': '50vh',
         'screen/3': 'calc(100vh / 3)',
         'screen/4': 'calc(100vh / 4)',
         'screen/5': 'calc(100vh / 5)',
         'screen/2-3': 'calc(100vh * 0.66)',
-      } )
+      })
     },
     fontFamily: {
-      sans: [ 'Space Grotesk', 'sans-serif' ],
-      serif: [ 'Merriweather', 'serif' ],
+      sans: ['Space Grotesk', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp')
+  ],
 }
