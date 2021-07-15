cc4c7ced7ad421d3a4d1d92616d9379c17a7c85b
Dimitrie Stefanescu <didimitrie@gmail.com>
2021-07-15 15:44:14 +0100

    feat: pricing page fiddles

--- tailwind.config.js
+++ tailwind.config.js
@@ -96,5 +96,6 @@ module.exports = {
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
   ],
 };
